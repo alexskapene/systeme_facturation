244f84b7563da15c3388bd8609bf8312f2ea2792
laurence1Mas <laurencemasika01@gmail.com>
2026-04-18 12:58:27 +0200

    feat: connect Product page to the Api

--- apps/frontend/src/store/store.ts
+++ apps/frontend/src/store/store.ts
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import clientReducer from './slices/clientSlice';
 import supplierReducer from './slices/supplierSlice';
+import productReducer from './slices/productSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     clients: clientReducer,
     suppliers: supplierReducer,
+    products: productReducer,
   },
 });
 
