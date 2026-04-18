fe48880559aed176a2f50bf2406f47e76f3da4bf
laurence1Mas <laurencemasika01@gmail.com>
2026-04-18 12:23:09 +0200

    feat: connect supplier page to the Api

--- apps/frontend/src/store/store.ts
+++ apps/frontend/src/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 // import authReducer from './slices/authSlice';
 import authReducer from './slices/authSlice';
 import clientReducer from './slices/clientSlice';
+import supplierReducer from './slices/supplierSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     clients: clientReducer,
+    suppliers: supplierReducer,
   },
 });
 
